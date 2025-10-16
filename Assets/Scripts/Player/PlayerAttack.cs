8ab783d25e45d568012faac711752ec202f0e17c
duyanh011204 <duyanh011204@gmail.com>
2025-10-16 18:29:59 +0700

    2D_Attack

--- Assets/Scripts/Player/PlayerAttack.cs
+++ Assets/Scripts/Player/PlayerAttack.cs
@@ -1,18 +1,58 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerAttack : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [Header("Attack Settings")]
+    [SerializeField] private float attackDamage = 20f;
+    [SerializeField] private float attackCooldown = 0.5f;
+    [SerializeField] private GameObject hitboxObject;
+
+    private bool canAttack = true;
+    private Animator animator;
+    private TopDownMovement movement;
+
     void Start()
     {
-        
+        animator = GetComponent<Animator>();
+        movement = GetComponent<TopDownMovement>();
+
+        if (hitboxObject != null)
+            hitboxObject.SetActive(false);
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetMouseButtonDown(0) && canAttack)
+            StartCoroutine(PerformAttack());
+    }
+
+    private IEnumerator PerformAttack()
+    {
+        canAttack = false;
+        animator.SetTrigger("Attack");
+
+        animator.SetFloat("LastHorizontal", movement.GetMovementDirection().x);
+        animator.SetFloat("LastVertical", movement.GetMovementDirection().y);
+
+        yield return new WaitForSeconds(attackCooldown);
+        canAttack = true;
+    }
+
+    public void EnableHitbox()
+    {
+        if (hitboxObject != null)
+            hitboxObject.SetActive(true);
+    }
+
+    public void DisableHitbox()
+    {
+        if (hitboxObject != null)
+            hitboxObject.SetActive(false);
+    }
+
+    public float GetAttackDamage()
+    {
+        return attackDamage;
     }
 }
