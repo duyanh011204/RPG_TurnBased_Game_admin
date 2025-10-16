95df08908baf4e6cf91f6a49b63bbe6a8fc32760
duyanh011204 <duyanh011204@gmail.com>
2025-10-16 18:31:04 +0700

    Update AttackHitbox.cs

--- Assets/Scripts/Player/AttackHitbox.cs
+++ Assets/Scripts/Player/AttackHitbox.cs
@@ -1,18 +1,23 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
-public class AttackHitbox : MonoBehaviour
+public class AttackHitBox : MonoBehaviour
 {
-    // Start is called before the first frame update
+    private PlayerAttack playerAttack;
+
     void Start()
     {
-        
+        playerAttack = GetComponentInParent<PlayerAttack>();
     }
 
-    // Update is called once per frame
-    void Update()
+    void OnTriggerEnter2D(Collider2D other)
     {
-        
+        if (other.CompareTag("Enemy"))
+        {
+            EnemyAI enemy = other.GetComponent<EnemyAI>();
+            if (enemy != null)
+            {
+                enemy.TakeDamage(playerAttack.GetAttackDamage());
+            }
+        }
     }
 }
